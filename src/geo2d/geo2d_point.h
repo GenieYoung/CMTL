c3194e4e87666ce67c3261073bf463748e88ff33
JLYang123 <yangjinlin@mail.ustc.edu.cn>
2024-03-14 13:57:51 +0800

    add some point operation

--- src/geo2d/geo2d_point.h
+++ src/geo2d/geo2d_point.h
@@ -51,13 +51,114 @@ class PointT
         const T& y() const { return _y; }
 
         /**
-         * @brief get the ith coordinate.
+         * @brief get the writable ith coordinate.
         */
         T& operator[](unsigned int i)
         {
             if(i == 0)  return _x;
             if(i == 1)  return _y;
             assert(false);
+        }
+
+        /**
+         * @brief get the const ith coordinate.
+        */
+        const T& operator[](unsigned int i) const
+        {
+            if(i == 0)  return _x;
+            if(i == 1)  return _y;
+            assert(false);
+        }
+
+    public:
+        /**
+         * @brief add two point.
+        */
+        PointT operator+(const PointT& p) const
+        {
+            return PointT(_x + p._x, _y + p._y);
+        }
+
+        /**
+         * @brief self-addition.
+        */
+        const PointT& operator+=(const PointT& p)
+        {
+            _x += p._x;
+            _y += p._y;
+            return *this;
+        }
+
+        /**
+         * @brief subtract two point.
+        */
+        PointT operator-(const PointT& p) const
+        {
+            return PointT(_x - p._x, _y - p._y);
+        }
+
+        /**
+         * @brief self-subtract.
+        */
+        const PointT& operator-=(const PointT& p)
+        {
+            _x -= p._x;
+            _y -= p._y;
+            return *this;
+        }
+
+        /**
+         * do scale by multiply.
+        */
+        PointT operator*(T scale) const
+        {
+            return PointT(_x * scale, _y * scale);
+        }
+
+        /**
+         * self-scale by multiply.
+        */
+        const PointT& operator*=(T scale)
+        {
+            _x *= scale;
+            _y *= scale;
+            return *this;
+        }
+
+        /**
+         * do scale by divide.
+        */
+        PointT operator/(T scale) const
+        {
+            assert(scale != 0);
+            return PointT(_x / scale, _y / scale);
+        }
+
+        /**
+         * self-scale by divide.
+        */
+        const PointT& operator/=(T scale)
+        {
+            assert(scale != 0);
+            _x /= scale;
+            _y /= scale;
+            return *this;
+        }
+
+        /**
+         * dot product.
+        */
+        T operator*(const PointT& p) const
+        {
+            return _x * p._x + _y * p._y;
+        }
+
+        /**
+         * cross product.
+        */
+        T operator%(const PointT& p) const
+        {
+            return _x * p._y - _y * p._x;
         }
 
     public:
--- test/geo2d_point_test.cpp
+++ test/geo2d_point_test.cpp
@@ -4,4 +4,5 @@ int main()
 {
     geo2d::PointT<double> p(0, 1);
     std::cout << p << std::endl;
+    std::cout << p[0] << " " << p[1] << std::endl;
 }
