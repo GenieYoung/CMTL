7e05483cbc627cc3888d22b29d1886f9382dbd82
JLYang123 <yangjinlin@mail.ustc.edu.cn>
2024-06-20 10:49:24 +0800

    complete import obj

--- src/CMTL/geometry/IO/import_obj.h
+++ src/CMTL/geometry/IO/import_obj.h
@@ -23,28 +23,16 @@ inline void trim_line_string(std::string& str)
         str = str.substr( start, end-start+1 );
 }
 
-/**
- * @brief build a surface mesh form .obj format file
- * @param sm surface mesh
- * @param file target .obj file position
- * @return true if sucessfully import, otherwise false
- */
 template<typename T>
-bool import_obj(geo3d::SurfaceMesh<T>& sm, const std::string& file)
+bool import_obj(geo3d::SurfaceMesh<T>& sm, std::istream& in)
 {
-    std::fstream in(file.c_str(), std::ios_base::in);
-    
-    if(!in.is_open() || !in.good())
-    {
-        std::cerr << "can not open file " << file << std::endl;
-        return false;
-    }
+    sm.clear();
 
     typedef typename geo3d::SurfaceMesh<T>::VertexHandle VertexHandle;
     typedef typename geo3d::SurfaceMesh<T>::Point        Point;
 
-    geo3d::SurfaceMesh<T> result;
     std::vector<VertexHandle> vhandles;
+    std::vector<VertexHandle> fvhs;
     float x, y, z;
 
     std::string line;
@@ -55,7 +43,7 @@ bool import_obj(geo3d::SurfaceMesh<T>& sm, const std::string& file)
         std::getline(in, line);
         if(in.bad())
         {
-            std::cerr << "read obj failed" << std::endl;
+            std::cerr << "error while reading obj line." << std::endl;
             return false;
         }
 
@@ -71,16 +59,73 @@ bool import_obj(geo3d::SurfaceMesh<T>& sm, const std::string& file)
         if(key == "v")
         {
             stream >> x; stream >> y; stream >> z;
-            if(!stream.fail())
-                vhandles.push_back(result.add_vertex(Point(x, y, z)));
+            if(stream.bad())
+            {
+                std::cerr << "error while reading obj vertex." << std::endl;
+                return false;
+            }
+            vhandles.push_back(sm.add_vertex(Point(x, y, z)));
         }
         else if(key == "f")
         {
+            int vid;
+            fvhs.clear();
+            while(stream >> vid)
+            {
+                if(vid > static_cast<int>(vhandles.size()) || vid == 0)
+                {
+                    std::cerr << "error while reading obj face." << std::endl;
+                    return false;
+                }
+                if(vid < 0)
+                    fvhs.push_back(vhandles[static_cast<int>(vhandles.size()) + vid]);
+                else
+                    fvhs.push_back(vhandles[vid-1]);
 
+                // we only read vertex in "v/vt/vn" format
+                stream.ignore(256, ' ');
+            }
+            if(stream.bad())
+            {
+                std::cerr << "error while reading obj face." << std::endl;
+                return false;
+            }
+            sm.add_face(fvhs);
+        }
+        else if(key == "vt")
+        {
+            // TODO
+        }
+        else if(key == "vn")
+        {
+            // TODO
         }
     }
+    
+    return !in.fail();
+}
 
-    return true;
+/**
+ * @brief build a surface mesh form .obj format file
+ * @param sm surface mesh
+ * @param file target .obj file position
+ * @return true if sucessfully import, otherwise false
+ */
+template<typename T>
+bool import_obj(geo3d::SurfaceMesh<T>& sm, const std::string& file)
+{
+    std::fstream in(file.c_str(), std::ios_base::in);
+    
+    if(!in.is_open() || !in.good())
+    {
+        std::cerr << "error while opening file " << file << std::endl;
+        return false;
+    }
+    
+    bool result = import_obj(sm, in);
+    in.close();
+
+    return result;
 }
 
 }   // namespace io
