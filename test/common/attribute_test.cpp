2711a35e6cab7e317e4ebda0ddf4a4fbac6f9e1f
GenieYoung <yangjinlin@mail.ustc.edu.cn>
2024-12-17 11:59:51 +0800

    add gtest

--- test/common/attribute_test.cpp
+++ test/common/attribute_test.cpp
@@ -1,16 +1,24 @@
 #include "CMTL/common/attributes.h"
 #include <iostream>
+#include <gtest/gtest.h>
 
-int main()
-{
-    CMTL::Attributes attr;
-    attr.set<int>("number") = 1;
-    attr.set<std::string>("string") = "hello";
-    std::cout << attr.get<int>("number") << " " << attr.get<std::string>("string") << std::endl;
-    attr.set<int>("number") = 2;
-    attr.set<std::string>("string") = "world";
-    std::cout << attr.get<int>("number") << " " << attr.get<std::string>("string") << std::endl;
-
-    //attr.set<std::string>("number") = "2";
-    return 0;
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
 }
+
+// int main()
+// {
+//     CMTL::Attributes attr;
+//     attr.set<int>("number") = 1;
+//     attr.set<std::string>("string") = "hello";
+//     std::cout << attr.get<int>("number") << " " << attr.get<std::string>("string") << std::endl;
+//     attr.set<int>("number") = 2;
+//     attr.set<std::string>("string") = "world";
+//     std::cout << attr.get<int>("number") << " " << attr.get<std::string>("string") << std::endl;
+
+//     //attr.set<std::string>("number") = "2";
+//     return 0;
+// }
