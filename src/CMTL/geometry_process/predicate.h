9da07f6f51c776c702f2f101ae48f6ddfcc48cd1
GenieYoung <yangjinlin@mail.ustc.edu.cn>
2024-12-24 16:13:43 +0800

    add predicate test

--- src/CMTL/geometry_process/predicate.h
+++ src/CMTL/geometry_process/predicate.h
@@ -13,15 +13,30 @@ namespace geometry_process{
  * @brief 2d orientation test, non-robust if point type is not exact.
  * @return return positive if the point pa, pb, and pc occur in counterclockwise order;
  *         negative if they occur in clockwise order; and on if they are collinear.
- * @note the point must have operator[] method to get coordinate
+ * @note the point must have value_type to represent number type and have operator[] method to get coordinate
  */
 template<typename Point>
 ORIENTATION orient_2d(const Point& pa, const Point& pb, const Point& pc)
 {
-    auto flag = (pa[0] - pc[0]) * (pb[1] - pc[1]) - (pa[1] - pc[1]) * (pb[0] - pc[0]);
-    if(is_greater(flag, decltype(flag)(0)))    
+    typename Point::value_type flag = (pa[0] - pc[0]) * (pb[1] - pc[1]) - (pa[1] - pc[1]) * (pb[0] - pc[0]);
+    if(is_greater(flag, typename Point::value_type(0)))    
         return ORIENTATION::POSITIVE;
-    else if(is_less(flag, decltype(flag)(0)))   
+    else if(is_less(flag, typename Point::value_type(0)))   
+        return ORIENTATION::NEGATIVE;
+    return ORIENTATION::ON;
+}
+
+/**
+ * @brief 2d orientation test, used for built-in arrays, non-robust if point type is not exact.
+ * @return return positive if the point pa, pb, and pc occur in counterclockwise order;
+ *         negative if they occur in clockwise order; and on if they are collinear.
+ */
+template<typename T>
+ORIENTATION orient_2d(const T (&pa)[2], const T (&pb)[2], const T (&pc)[2]) {
+    T flag = (pa[0] - pc[0]) * (pb[1] - pc[1]) - (pa[1] - pc[1]) * (pb[0] - pc[0]);
+    if (is_greater(flag, T(0)))
+        return ORIENTATION::POSITIVE;
+    else if (is_less(flag, T(0)))
         return ORIENTATION::NEGATIVE;
     return ORIENTATION::ON;
 }
@@ -51,6 +66,28 @@ ORIENTATION orient_3d(const Point& pa, const Point& pb, const Point& pc, const P
 }
 
 /**
+ * @brief 3d orientation test, used for built-in arrays
+ * @return return positive if the point pd lies above the plane passing through pa, pb, and pc,
+ *         "above" is defined so that pa, pb, and pc appear in counterclockwise order when viewed from above the plane,
+ *         return on if the points are coplanar, otherwise return negative.
+ */
+template<typename T>
+ORIENTATION orient_3d(const T (&pa)[3], const T (&pb)[3], const T (&pc)[3], const T (&pd)[3])
+{
+    T adx = pd[0] - pa[0], ady = pd[1] - pa[1], adz = pd[2] - pa[2],
+      abx = pb[0] - pa[0], aby = pb[1] - pa[1], abz = pb[2] - pa[2],
+      acx = pc[0] - pa[0], acy = pc[1] - pa[1], acz = pc[2] - pa[2];
+    T flag = adx * (aby * acz - abz * acy) +
+             ady * (abz * acx - abx * acz) +
+             adz * (abx * acy - aby * acx);
+    if(is_greater(flag, T(0)))
+        return ORIENTATION::POSITIVE;
+    else if(is_less(flag, T(0)))
+        return ORIENTATION::NEGATIVE;
+    return ORIENTATION::ON;
+}
+
+/**
  * @brief check whether two 2d triangle [pa, pb, pc] and [pb, pa, pd] is locally delaunay.
  * @tparam Point 2d point
  * @return true if locally delaunay, otherwise false
@@ -59,10 +96,26 @@ ORIENTATION orient_3d(const Point& pa, const Point& pb, const Point& pc, const P
 template<typename Point>
 bool is_locally_delaunay(const Point& pa, const Point& pb, const Point& pc, const Point& pd)
 {
-    std::array<typename Point::value_type, 3> pa_ = {pa[0], pa[1], pa[0]*pa[0] + pa[1]*pa[1]};
-    std::array<typename Point::value_type, 3> pb_ = {pb[0], pb[1], pb[0]*pb[0] + pb[1]*pb[1]};
-    std::array<typename Point::value_type, 3> pc_ = {pc[0], pc[1], pc[0]*pc[0] + pc[1]*pc[1]};
-    std::array<typename Point::value_type, 3> pd_ = {pd[0], pd[1], pd[0]*pd[0] + pd[1]*pd[1]};
+    typename Point::value_type pa_[3] = {pa[0], pa[1], pa[0]*pa[0] + pa[1]*pa[1]};
+    typename Point::value_type pb_[3] = {pb[0], pb[1], pb[0]*pb[0] + pb[1]*pb[1]};
+    typename Point::value_type pc_[3] = {pc[0], pc[1], pc[0]*pc[0] + pc[1]*pc[1]};
+    typename Point::value_type pd_[3] = {pd[0], pd[1], pd[0]*pd[0] + pd[1]*pd[1]};
+    ORIENTATION flag = orient_3d(pa_, pb_, pc_, pd_);
+    return flag == ORIENTATION::POSITIVE || flag == ORIENTATION::ON;
+}
+
+/**
+ * @brief check whether two 2d triangle [pa, pb, pc] and [pb, pa, pd] is locally delaunay, used for built-in arrays
+ * @tparam Point 2d point
+ * @return true if locally delaunay, otherwise false
+ */
+template<typename T>
+bool is_locally_delaunay(const T (&pa)[2], const T (&pb)[2], const T (&pc)[2], const T (&pd)[2])
+{
+    T pa_[3] = {pa[0], pa[1], pa[0]*pa[0] + pa[1]*pa[1]};
+    T pb_[3] = {pb[0], pb[1], pb[0]*pb[0] + pb[1]*pb[1]};
+    T pc_[3] = {pc[0], pc[1], pc[0]*pc[0] + pc[1]*pc[1]};
+    T pd_[3] = {pd[0], pd[1], pd[0]*pd[0] + pd[1]*pd[1]};
     ORIENTATION flag = orient_3d(pa_, pb_, pc_, pd_);
     return flag == ORIENTATION::POSITIVE || flag == ORIENTATION::ON;
 }
--- test/geometry_process/predicate_test.cpp
+++ test/geometry_process/predicate_test.cpp
@@ -2,37 +2,134 @@
 #include "CMTL/geo3d/geo3d_point.h"
 #include "CMTL/geometry_process/predicate.h"
 
+#include <gtest/gtest.h>
 
-void orient2d_test()
+typedef CMTL::geo2d::Point<double> Point2D;
+typedef CMTL::geo2d::Point<mpq_class> Point2R;
+typedef CMTL::geo3d::Point<double> Point3D;
+typedef CMTL::geo3d::Point<mpq_class> Point3R;
+
+using namespace CMTL;
+using namespace CMTL::geometry_process;
+
+TEST(PredicateTest, Orient2dTest)
 {
-    typedef CMTL::geo2d::Point<double> PointF;
-    PointF p1(0,0), p2(1,0), p3(0.5, -0.00001);
-    std::cout << p1 << " " << p2 << " " << p3 << " "
-              << CMTL::geometry_process::orient_2d(p1, p2, p3) << std::endl;
+    Point2D pd1(0, 0), pd2(1, 6);
+    EXPECT_TRUE(orient_2d(pd1, pd2, {0.5, 3+1e-10})==ORIENTATION::ABOVE);
+    EXPECT_TRUE(orient_2d(pd1, pd2, {0.5, 3-1e-10})==ORIENTATION::BELOW);
+    EXPECT_TRUE(orient_2d(pd1, pd2, {0.5, 3})==ORIENTATION::ON);
+    
+    double d1[2] = {0, 0};
+    double d2[2] = {1, 6};
+    double d3[2] = {0.5, 3+1e10};
+    double d4[2] = {0.5, 3-1e10};
+    double d5[2] = {0.5, 3};
+    EXPECT_TRUE(orient_2d(d1, d2, d3)==ORIENTATION::ABOVE);
+    EXPECT_TRUE(orient_2d(d1, d2, d4)==ORIENTATION::BELOW);
+    EXPECT_TRUE(orient_2d(d1, d2, d5)==ORIENTATION::ON);
+
+
+    Point2R pr1(0, 0), pr2(1, 6);
+    EXPECT_TRUE(orient_2d(pr1, pr2, {0.5, mpq_class(1, 1e100)+3})==ORIENTATION::ABOVE);
+    EXPECT_TRUE(orient_2d(pr1, pr2, {0.5, -mpq_class(1, 1e100)+3})==ORIENTATION::BELOW);
+    EXPECT_TRUE(orient_2d(pr1, pr2, {0.5, 3})==ORIENTATION::ON);
+
+    mpq_class r1[2] = {0, 0};
+    mpq_class r2[2] = {1, 6};
+    mpq_class r3[2] = {0.5, mpq_class(1, 1e100) + 3};
+    mpq_class r4[2] = {0.5, -mpq_class(1, 1e100) + 3};
+    mpq_class r5[2] = {0.5, 3};
+    EXPECT_TRUE(orient_2d(r1, r2, r3)==ORIENTATION::ABOVE);
+    EXPECT_TRUE(orient_2d(r1, r2, r4)==ORIENTATION::BELOW);
+    EXPECT_TRUE(orient_2d(r1, r2, r5)==ORIENTATION::ON);
 }
 
-void orient3d_test()
+TEST(PredicateTest, Orient3dTest)
 {
-    typedef CMTL::geo3d::Point<double> PointF;
-    PointF p1(0,0,0), p2(1,0,1), p3(0,1,0), p4(0.5, 0.5, 0.500001);
-    std::cout << p1 << " " << p2 << " " << p3 << " " << p4 << " "
-              << CMTL::geometry_process::orient_3d(p1, p2, p3, p4) << std::endl;
+    Point3D pd1(0, 1, 0), pd2(0, 0, 0), pd3(3, 0, 7);
+    EXPECT_TRUE(orient_3d(pd1, pd2, pd3, {1.5, 0.5, 3.5+1e-10})==ORIENTATION::ABOVE);
+    EXPECT_TRUE(orient_3d(pd1, pd2, pd3, {1.5, -0.5, 3.5-1e-10})==ORIENTATION::BELOW);
+    EXPECT_TRUE(orient_3d(pd1, pd2, pd3, {1.5, 0, 3.5})==ORIENTATION::ON);
+    
+    double d1[3] = {0, 1, 0};
+    double d2[3] = {0, 0, 0};
+    double d3[3] = {3, 0, 7};
+    double d4[3] = {1.5, 0.5, 3.5+1e10};
+    double d5[3] = {1.5, -0.5, 3.5-1e10};
+    double d6[3] = {1.5, 0, 3.5};
+    EXPECT_TRUE(orient_3d(d1, d2, d3, d4)==ORIENTATION::ABOVE);
+    EXPECT_TRUE(orient_3d(d1, d2, d3, d5)==ORIENTATION::BELOW);
+    EXPECT_TRUE(orient_3d(d1, d2, d3, d6)==ORIENTATION::ON);
+
+    Point3R pr1(0, 1, 0), pr2(0, 0, 0), pr3(3, 0, 7);
+    EXPECT_TRUE(orient_3d(pr1, pr2, pr3, {1.5, 0.5, mpq_class(1, 1e100)+3.5})==ORIENTATION::ABOVE);
+    EXPECT_TRUE(orient_3d(pr1, pr2, pr3, {1.5, -0.5, -mpq_class(1, 1e100)+3.5})==ORIENTATION::BELOW);
+    EXPECT_TRUE(orient_3d(pr1, pr2, pr3, {1.5, 0, 3.5})==ORIENTATION::ON);
+
+    mpq_class r1[3] = {0, 1, 0};
+    mpq_class r2[3] = {0, 0, 0};
+    mpq_class r3[3] = {3, 0, 7};
+    mpq_class r4[3] = {1.5, 0.5, mpq_class(1, 1e100)+3.5};
+    mpq_class r5[3] = {1.5, -0.5, -mpq_class(1, 1e100)+3.5};
+    mpq_class r6[3] = {1.5, 0, 3.5};
+    EXPECT_TRUE(orient_3d(r1, r2, r3, r4)==ORIENTATION::ABOVE);
+    EXPECT_TRUE(orient_3d(r1, r2, r3, r5)==ORIENTATION::BELOW);
+    EXPECT_TRUE(orient_3d(r1, r2, r3, r6)==ORIENTATION::ON);
 }
 
-void locally_delaunay_test()
+TEST(PredicateTest, LocalDelaunayTest)
 {
-    typedef CMTL::geo2d::Point<double> PointF;
-    PointF p1(-2.88,-5.32), p2(-2.72,-5.67), p3(-2.70, -5.37), p4(-2.67, -6.03);
-    //PointF p1(0,0), p2(10,0), p3(5, 5), p4(5, -4);
-    std::cout << p1 << " " << p2 << " " << p3 << " " << p4 << " "
-              << (CMTL::geometry_process::is_locally_delaunay(p1, p2, p3, p4) ? "delaunay" : "not delaunay") << std::endl;
+    Point2D pd1(1, 0), pd2(0, 1), pd3(0, 0), pd4(1, 1);
+    EXPECT_TRUE(is_locally_delaunay(pd1, pd2, pd3, pd4));
+    EXPECT_TRUE(is_locally_delaunay(pd1, pd2, pd3, pd4+Point2D(1e-10, 1e-10)));
+    EXPECT_FALSE(is_locally_delaunay(pd1, pd2, pd3, pd4-Point2D(1e-10, 1e-10)));
+
+    double d1[2] = {1, 0};
+    double d2[2] = {0, 1};
+    double d3[2] = {0, 0};
+    double d4[2] = {1, 1};
+    double d5[2] = {1+1e-10, 1+1e-10};
+    double d6[2] = {1-1e-10, 1-1e-10};
+    EXPECT_TRUE(is_locally_delaunay(d1, d2, d3, d4));
+    EXPECT_TRUE(is_locally_delaunay(d1, d2, d3, d5));
+    EXPECT_FALSE(is_locally_delaunay(d1, d2, d3, d6));
+
+    Point2R pr1(1, 0), pr2(0, 1), pr3(0, 0), pr4(1, 1);
+    EXPECT_TRUE(is_locally_delaunay(pr1, pr2, pr3, pr4));
+    EXPECT_TRUE(is_locally_delaunay(pr1, pr2, pr3, pr4+Point2D(mpq_class(1,1e100), mpq_class(1,1e100))));
+    EXPECT_FALSE(is_locally_delaunay(pr1, pr2, pr3, pr4-Point2D(mpq_class(1,1e100), mpq_class(1,1e100))));
 }
 
-int main()
-{
-    orient2d_test();
-    orient3d_test();
-    locally_delaunay_test();
-}
+// void orient2d_test()
+// {
+//     typedef CMTL::geo2d::Point<double> PointF;
+//     PointF p1(0,0), p2(1,0), p3(0.5, -0.00001);
+//     std::cout << p1 << " " << p2 << " " << p3 << " "
+//               << CMTL::geometry_process::orient_2d(p1, p2, p3) << std::endl;
+// }
+
+// void orient3d_test()
+// {
+//     typedef CMTL::geo3d::Point<double> PointF;
+//     PointF p1(0,0,0), p2(1,0,1), p3(0,1,0), p4(0.5, 0.5, 0.500001);
+//     std::cout << p1 << " " << p2 << " " << p3 << " " << p4 << " "
+//               << CMTL::geometry_process::orient_3d(p1, p2, p3, p4) << std::endl;
+// }
+
+// void locally_delaunay_test()
+// {
+//     typedef CMTL::geo2d::Point<double> PointF;
+//     PointF p1(-2.88,-5.32), p2(-2.72,-5.67), p3(-2.70, -5.37), p4(-2.67, -6.03);
+//     //PointF p1(0,0), p2(10,0), p3(5, 5), p4(5, -4);
+//     std::cout << p1 << " " << p2 << " " << p3 << " " << p4 << " "
+//               << (CMTL::geometry_process::is_locally_delaunay(p1, p2, p3, p4) ? "delaunay" : "not delaunay") << std::endl;
+// }
+
+// int main()
+// {
+//     orient2d_test();
+//     orient3d_test();
+//     locally_delaunay_test();
+// }
 
 
